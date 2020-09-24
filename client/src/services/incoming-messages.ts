089c1e067e59f86daf1d3c55ed1816787a87ccc3
Caleb Gregory <calebgregory@gmail.com>
2020-09-24 07:36:42 -0500

    Groups incoming COMM messages by id; scans messages and outputs array of messages for view

--- client/src/services/incoming-messages.ts
+++ client/src/services/incoming-messages.ts
@@ -1,11 +1,7 @@
-import { Observable, from, merge } from 'rxjs'
+import { Observable, merge } from 'rxjs'
 import { Service } from 'src/util/service'
-import { filter, map, scan, mergeMap, tap } from 'rxjs/operators'
+import { filter, map, scan } from 'rxjs/operators'
 import { CommMessage, JoinedMessage, LeftMessage, Message } from 'src/message/message'
-
-import { logger } from 'src/logger'
-
-const log = logger('incoming-messages')
 
 interface Dependencies {
   incoming$: Observable<Message>
@@ -19,39 +15,21 @@ export function filterMessageType<MessageType>(messageType: string) {
   );
 }
 
+// @@TODO: name this better
 export function groupByDataId() {
-  return (source$: Observable<CommMessage>) => source$.pipe(
+  return (source$: Observable<Message>) => source$.pipe(
     scan((messages, msg) => {
-      // JavaScript Maps preserve insertion order, so message-order will be retained
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
-      return messages.set(msg.data.id, msg)
-    }, new Map<string, Message>()),
-    mergeMap((messages: Map<string, Message>) => from(messages.values()))
-  )
-}
-
-export function groupByDataId2() {
-  return (source$: Observable<CommMessage>) => {
-    let messagesByDataId = new Map()
-    return new Observable((observer) => {
-      const srcSubscription = source$.subscribe({
-        next(msg: CommMessage) {
-          messagesByDataId.set(msg.data.id, msg)
-          observer.next(msg)
-        },
-        error(err) {
-          observer.error(err)
-        },
-        complete() {
-          observer.complete()
-        }
-      })
-      return () => {
-        srcSubscription.unsubscribe()
-        messagesByDataId = new Map()
+      // overwrite existing COMM message with new one
+      // e.g., [PROGRESS, p] -> [PROGRESS, pr] -> [PROGRESS, pro] -> [COMPLETE, pro]
+      if (msg.type === 'COMM') {
+        return messages.set(msg.data.id, msg)
       }
-    })
-  }
+      return messages.set(msg.id, msg)
+      // because JavaScript Maps preserve insertion order, message-order will be retained
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+    }, new Map<string, Message>()),
+    map((messages: Map<string, Message>) => Array.from(messages.values()))
+  )
 }
 
 export class IncomingMessages extends Service {
@@ -64,15 +42,10 @@ export class IncomingMessages extends Service {
     const _leftMessage$: Observable<LeftMessage> = incoming$.pipe(filterMessageType<LeftMessage>('LEFT'))
 
     const _commMessage$: Observable<CommMessage> = incoming$.pipe(filterMessageType<CommMessage>('COMM'))
-    // const _commMessages$ = _commMessage$.pipe(
-    //   groupByDataId()
-    // )
 
     this.messages$ = merge(_joinMessage$, _commMessage$, _leftMessage$)
       .pipe(
-        tap((message: Message) => log.info('message', { message })),
-        scan((acc: Message[], curr: Message) => [...acc, curr], []),
-        tap((messages: Message[]) => log.info('messages', { messages }))
+        groupByDataId(),
       )
   }
 }
